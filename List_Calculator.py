78dcce92c8d92487b81d38f7b5260ea10baa02a8
Jishnu Suresh <109917899+Jishnu-JSK@users.noreply.github.com>
2022-12-07 21:03:08 +0530

    Add files via upload

---
+++ List_Calculator.py
@@ -0,0 +1,60 @@
+#Four Number Calculator
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+c=[a+b,a-b,a*b,a/b,a//b]
+d=int(input('''Menu 1
+ADD 0
+SUB 1
+MULTI 2
+DIVIDE(Floor) 3
+DIVIDE(Integer) 4
+Enter your choice:
+'''))
+e=c[d]
+f=int(input("Enter third number:"))
+g=int(input("Enter fourth number:"))
+h=[f+g,f-g,f*g,f/g,f//g]
+i=int(input('''Menu 2
+ADD 0
+SUB 1
+MULTI 2
+DIVIDE(Floor) 3
+DIVIDE(INTEGER) 4
+Enter your choice:
+'''))
+j=h[i]
+z=input('''Enter the operation to combine the four numbers:
+ADD 
+SUB
+MULTI
+FLOOR
+INT
+Enter your choice:
+''')
+if z in ["ADD","Add","add"]:
+    x=e+j
+    print("Output:",x)
+elif z in ["SUB","Sub","sub"]:
+    y=e-j
+    print("Output:",y)
+elif z in ["MULTI","Multi","multi"]:
+    w=e*j
+    print("Output:",w)
+elif z in ["FLOOR","Floor","floor"]:
+    l=e/j
+    print("Output:",l)
+elif z in ["INT","Int","int"]:
+    q=e//j
+    print("Output:",q)
+else:
+    pass
+
+
+
+
+
+
+
+
+
+
